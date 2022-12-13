f0feba10f85e9cf2cfb3513ca8c7227d620f8b3e
lilcryptotech <72393350+seandadonntech@users.noreply.github.com>
2022-12-12 19:32:53 -0500

    Create reloop.py

---
+++ reloop/reloop.py
@@ -0,0 +1,13 @@
+
+
+def main():
+ name = input('hello user type enter to continue\n')
+main()
+
+
+
+reloop = input('would you like to loop this program again?\n')
+if reloop == 'y':
+ main()
+else:
+ print('exting program')
